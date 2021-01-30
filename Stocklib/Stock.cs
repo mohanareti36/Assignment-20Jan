0bb4038302c87edd37a7f323897d4adac789b764
mohan areti <mohan.areti@zensar.com>
2021-01-30 13:19:42 +0530

    Add project files.

---
+++ Stocklib/Stock.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stocklib
+{
+    public enum StockType
+    {
+        Equity,
+        MutualFund
+    }
+    public class Stock
+    {
+        public string name;
+        public double price;
+        public int qty;
+        public StockType type;
+        public string Name { get; set; }
+        public int Qty { get; set; }
+        public double Price { get; }
+        public StockType Type { get; }
+        public Stock(string name, double price)
+        {
+            Name = name;
+            Price = price;
+            Qty = 100;
+            Type = StockType.Equity;
+        }
+        public Stock(string name, double price, int qty, StockType type)
+        {
+            Name = name;
+            Price = price;
+            Qty = qty;
+            Type = type;
+        }
+
+        public class Broker
+        {
+
+            List<Stock> portfolio = new List<Stock>(5);
+
+            public void BuyStock(Stock stk)
+            {
+                portfolio.Add(stk);
+            }
+
+            public void ShowPortfolio()
+            {
+                foreach (var item in portfolio)
+                {
+                    Console.WriteLine($"Name:{item.Name}\nQuantity:{item.Qty} Price:{item.Price}\n Type:{item.Type}");
+                    Console.WriteLine("----------------------------------------------------------------------------");
+                }
+                double networth = 0;
+                IEnumerator<Stock> e = portfolio.GetEnumerator();
+                while (e.MoveNext())
+                {
+                    networth = networth + e.Current.Price * e.Current.Qty;
+                }
+                Console.WriteLine($"Networth is:{networth}");
+
+            }
+        }
+
+    }
+}
---
+++ Stockmain/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stocklib;
+using static Stocklib.Stock;
+
+namespace Stockmain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stock st1 = new Stock("TATA", 2500);
+            Stock st2 = new Stock("Mahindra", 1000, 100, StockType.MutualFund);
+            Stock st3 = new Stock("Hyundai", 3000, 200, StockType.MutualFund);
+            Stock st4 = new Stock("Kia", 4000, 200, StockType.MutualFund);
+            Stock st5 = new Stock("BMW", 6000);
+            Broker br = new Broker();
+            br.BuyStock(st1);
+            br.BuyStock(st2);
+            br.BuyStock(st3);
+            br.BuyStock(st4);
+           // br.BuyStock(st5);
+
+            br.ShowPortfolio();
+
+        }
+    }
+}
